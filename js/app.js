8248a4d7c5fd27ab5e5b343bc551cc6421b70459
SouravBasak-Alien <sb07008@gmail.com>
2022-07-27 04:54:03 +0600

    10 minute school

---
+++ js/app.js
@@ -0,0 +1,191 @@
+$(document).ready(function () {
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		appendDots: '.slider-dots',
+		dots: true,
+		dotsClass: 'dots',
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					centerPadding: '10px',
+					slidesToShow: 2,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+
+					slidesToShow: 1,
+
+				}
+			}
+		]
+
+	})
+
+
+	$('#job-warper').slick({
+
+		arrows: false,
+		centerMode: true,
+		centerPadding: '100px',
+		slidesToShow: 3,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '10px',
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '10px',
+					slidesToShow: 1
+				}
+			}
+		]
+
+	})
+	$('.category-slider').slick({
+		infinite: false,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '10px',
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			}
+		]
+	})
+
+
+
+	$('#skill-warper').slick({
+
+		infinite: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '10px',
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '10px',
+					slidesToShow: 1
+				}
+			}
+		]
+
+	})
+
+
+
+	$('.client-warper').slick({
+		infinite: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '20px',
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '20px',
+					slidesToShow: 1
+				}
+			}
+		]
+
+	})
+
+	$('.content-warper').slick({
+		infinite: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '10px',
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '10px',
+					slidesToShow: 1
+				}
+			}
+		]
+
+	})
+
+
+
+
+
+
+});
+const iconMenu = document.getElementById("mobile-hambarger");
+const menu = document.getElementById("mobile-menu");
+
+iconMenu.addEventListener("click", function () {
+	menu.classList.add("open")
+})
+iconMenu.addEventListener("click", function () {
+	menu.classList.remove("open")
+})
